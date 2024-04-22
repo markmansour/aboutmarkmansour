d0cdd5890ea73c063064927738f4990809c7d4cf
Mark Mansour <mark@stateofflux.com>
2024-04-22 16:47:16 -0700

    Using domain aboutmarkmansour.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ const whenExternalScripts = (items = []) =>
 
 export default defineConfig({
   output: 'static',
-  site: 'https://stateofflux.com',
+  site: 'https://aboutmarkmansour.com',
 //   base: '/',
 
   integrations: [
